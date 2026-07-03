a029c913ac5a18aa4a5430a4e950a73de9674440
Taiki <t.motose@motion-plan.co.jp>
2026-07-03 20:23:00 +0900

    Add Japanese font

--- report.py
+++ report.py
@@ -1,58 +1,19 @@
-from reportlab.platypus import *
-from reportlab.lib.styles import getSampleStyleSheet
+import os
+
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
-pdfmetrics.registerFont(
-    TTFont(
-        "Meiryo",
-        "C:/Windows/Fonts/meiryo.ttc"
-    )
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+font_path = os.path.join(
+    BASE_DIR,
+    "fonts",
+    "NotoSansJP-Regular.ttf"
 )
 
-def create_report(
-    filename,
-    moss_ratio,
-    moss_area,
-    score,
-    rank,
-    comment
-):
-
-    doc = SimpleDocTemplate(filename)
-
-    style = getSampleStyleSheet()["Normal"]
-
-    style.fontName = "Meiryo"
-
-    elements = []
-
-    elements.append(
-        Paragraph("<b>屋根AI診断レポート</b>", style)
+pdfmetrics.registerFont(
+    TTFont(
+        "NotoJP",
+        font_path
     )
-
-    elements.append(Spacer(1,20))
-
-    elements.append(
-        Paragraph(f"コケ率：{moss_ratio:.1f}%", style)
-    )
-
-    elements.append(
-        Paragraph(f"コケ面積：{moss_area:.1f}㎡", style)
-    )
-
-    elements.append(
-        Paragraph(f"健康スコア：{score:.0f}", style)
-    )
-
-    elements.append(
-        Paragraph(f"判定：{rank}", style)
-    )
-
-    elements.append(Spacer(1,20))
-
-    elements.append(
-        Paragraph(comment, style)
-    )
-
-    doc.build(elements)
+)
